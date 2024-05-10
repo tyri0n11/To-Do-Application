f6380994ae70060b0a90c0c46ca201f05fabbb34
Châu Thịnh <chauthinh00710@gmail.com>
2024-05-10 22:15:19 +0700

    build: project

---
+++ script.js
@@ -0,0 +1,35 @@
+const input = document.getElementById("input-box")
+const listContainer = document.getElementById("list-container")
+
+function addTask(){
+    if(input.value === ''){
+        alert("You must write something!")
+    }else{
+        let li = document.createElement("li")
+        li.innerHTML = input.value
+        listContainer.appendChild(li)
+        let span = document.createElement("span")
+        span.innerHTML = "\u00d7"
+        li.appendChild(span)
+    }
+    input.value =""
+    save()
+}
+listContainer.addEventListener("click", function (e){
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+        save()
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove()
+        save()
+    }
+}, false)
+
+function save(){
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask()
